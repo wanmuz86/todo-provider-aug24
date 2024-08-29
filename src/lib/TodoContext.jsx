75a928a7d7693d7bd08080eba6d9d8ac24972ee8
Wan Muzaffar <wanmuz86@gmail.com>
2024-08-29 11:48:47 +0800

    complete add_todo and remove_todo

--- src/lib/TodoContext.jsx
+++ src/lib/TodoContext.jsx
@@ -10,6 +10,34 @@ const TodoProvider = ({children}) => {
 
     // Define the states that going to be shared to the components, within this context
 
+    const initialState = {
+        todos:[]
+    }
+
+    const todoReducer = (state, action)=>{
+        switch (action.type){
+            case 'ADD_TODO':
+
+            // spread operator to modify the state object
+            // spread operator to add a new todo at the end of array todos
+            return {
+                ...state,
+                todos:[...state.todos, action.payload] //action.payload {id:(timestamp),name:string}
+            };
+
+            case 'REMOVE_TODO':
+
+            return  {
+                ...state, 
+                todos: state.todos.filter(val=>val.id!== action.payload) // action.payload {id:timestamp}
+            };
+
+            default: // Default state will be called by TodoList component
+
+            return state
+        }
+    }
+
 };
 
 export {TodoProvider}
